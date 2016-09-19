84f4e8a6f198aa175b77e3b861cff439122610f1
sabertazimi <sabertazimi@gmail.com>
2016-09-19 22:58:52 +0800

    update(os-dinux-string libs): implement strcmp/strcpy/strcat/strlen libs
    issue sabertazimi/awesome-notes#27

--- libs/string.c
+++ libs/string.c
@@ -8,18 +8,42 @@
 #include <string.h>
 
 inline int strcmp(const char *str1, const char *str2) {
+    while (*str1 != '\0' && *str1 == *str2) {
+        ;
+    }
 
+    return (int)(*str1 - *str2);
 }
 
 inline char *strcpy(char *dest, const char *src) {
+    while (*src) {
+        *dest++ = *src++;
+    }
 
+    *dest = '\0';
+    return dest;
 }
 
 inline char *strcat(char *dest, const char *src) {
+    while (*dest) {
+        ;
+    }
 
+    while (*src) {
+        *dest++ = *src++;
+    }
+
+    *dest = '\0';
+    return dest;
 }
 
 inline int strlen(const char *src) {
+    int nr_char = 0;
 
+    while (*src) {
+        nr_char++;
+    }
+
+    return nr_char;
 }
 
